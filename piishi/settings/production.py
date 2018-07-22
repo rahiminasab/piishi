7be41f3c1a428bd52c5b2581750fb12ed590d8e3
Ehsan RahimiNasab <rahiminasabe@gmail.com>
2018-07-22 11:42:41 -0400

    secret key changed and stored as env variable

--- piishi/settings/production.py
+++ piishi/settings/production.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__fil
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ')9g%8t1kt(&%2i@f+5fs#umo3p%5+!xxov%a07xn3o0@zo7yz^'
+SECRET_KEY = os.environ['DJANGO_PIISHI_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
